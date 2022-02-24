47e17cf248816b7533dec996a40d917486f665b0
Matty <53088136+byronbytes@users.noreply.github.com>
2022-02-24 12:45:26 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
   
-    public static void main(String[] args) {
-       System.out.println("Hello, World!");
-      // Yay, it does a thing!
+    public static void main(String[] args) 
+    {
+      Scanner input = new Scanner(System.in);
+      
+      boolean otherText = true;
+      System.out.println("Hello, World!");    // Yay, it does a thing!
+      System.out.println("OtherText is set to: " + otherText); 
     }
   
 }
